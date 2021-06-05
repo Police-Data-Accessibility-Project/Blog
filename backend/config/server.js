5c0ea1c80334206a4119ce42d92fd784dc8f2e9a
Eric Turner <me@ericturner.it>
2021-06-04 23:21:22 -0400

    add url to backend server

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL', '0.0.0.0'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '851a4b64db6c202bf571e57e34c96a83'),
